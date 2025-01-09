2368e66c3a56f51cc39398152d19e84e4a69b68c
FrenyChauhan <chauhanfreny01@gmail.com>
2025-01-09 15:08:34 +0530

    add gitignore and front end files

---
+++ core/weather_app/static/script.js
@@ -0,0 +1,18 @@
+const userLocation=document.querySelector("#userLocation");
+const converter=document.querySelector("#converter");
+const temperature=document.querySelector(".temperature");
+const feelsLike=document.querySelector(".feelsLike");
+const description=document.querySelector(".description");
+const date=document.querySelector(".date");
+const city=document.querySelector(".city");
+const HValue=document.querySelector("#HValue");
+const WValue=document.querySelector("#WValue");
+const SRValue=document.querySelector("#SRValue");
+const SSValue=document.querySelector("#SSValue");
+const CValue=document.querySelector("#CValue");
+const UVValue=document.querySelector("#UVValue");
+const PValue=document.querySelector("#PValue");
+const Forecast =document.querySelector(".Forecast");
+
+
+console.log(userLocation);
